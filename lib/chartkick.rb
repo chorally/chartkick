17df3a95d57498ee55536e4f3a30b1e17f528089
Francesco Lupano <lupano.web90@gmail.com>
2017-01-05 15:08:12 +0100

    Update chartkick.rb

--- lib/chartkick.rb
+++ lib/chartkick.rb
@@ -15,11 +15,7 @@ end
 # use Enumerable so it can be called on arrays
 module Enumerable
   def chart_json
-    if is_a?(Hash) && (key = keys.first) && key.is_a?(Array) && key.size == 3
-      group_by { |k, a, _v| k[0] }.map do |name, activity, data|
-        {name: name, activity: activity, data: data.map { |k, v| [k[1], v] }}
-      end
-    elsif is_a?(Hash) && (key = keys.first) && key.is_a?(Array) && key.size == 2
+    if is_a?(Hash) && (key = keys.first) && key.is_a?(Array) && key.size == 2
       group_by { |k, _v| k[0] }.map do |name, data|
         {name: name, data: data.map { |k, a, v| [k[1], v] }}
       end
